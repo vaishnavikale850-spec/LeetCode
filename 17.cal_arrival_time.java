a80c3d338437268176e77cbf9a0e32edad13d9a2
vaishnavikale850-spec <vaishnavikale850@gmail.com>
2026-05-01 14:57:04 +0530

    File added

---
+++ 17.cal_arrival_time.java
@@ -0,0 +1,23 @@
+import java.util.*;
+class Solution {
+	public static void main(String[]args){
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter Arrival Time : ");
+		int arrivalTime = sc.nextInt();
+		System.out.print("Enter Delayed Time : ");
+		int delayedTime = sc.nextInt();
+		int op = findDelayedArrivalTime(arrivalTime, delayedTime);
+		System.out.print("Output : "+ op);
+	}
+    public static int findDelayedArrivalTime(int arrivalTime, int delayedTime) {
+        int op = 0;
+        int digit = arrivalTime + delayedTime;
+        if(digit >= 24){
+            return digit%24;
+        }
+        else{
+            return digit;
+        }
+
+    }
+}
